7eb08c0b2711cb61f66f63fe54387213b41e5493
Gregory <g.tineo11@gmail.com>
2017-09-02 09:56:58 -0400

    delete

--- resources/views/layouts/footer.blade.php
+++
@@ -1,9 +0,0 @@
-  <!-- Main Footer -->
-  <footer class="main-footer" >
-    <!-- To the right -->
-    <div class="pull-right hidden-xs">
-     	Version 1.0
-    </div>
-    <!-- Default to the left -->
-    <strong>Copyright &copy; 2017-{{ date('Y') }}, <a href="#">Innovaciones Tecnologicas T&T, C.A</a>.</strong> Todos los derechos reservados.
-  </footer>
